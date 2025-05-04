86379825cfe8771ce8f13b4d979c2a3590ed8601
NADU-v <Mybotmd1@gmail.com>
2025-05-04 09:21:54 +0530

    Update and rename plugins/alive (10).js to msg.js

---
+++ msg.js
@@ -0,0 +1,84 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')z
+cmd({
+    pattern: "ping",
+    react: "⚡",
+    alias: ["speed"],
+    desc: "Check bot\'s ping",
+    category: "main",
+    use: '.ping',
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+var inital = new Date().getTime();
+let ping = await conn.sendMessage(from , { text: '```Pinging To index.js!!!```'  }, { quoted: mek } )
+var final = new Date().getTime();
+return await conn.edit(ping, '*Pong*\n *' + (final - inital) + ' ms* ' )
+} catch (e) {
+reply(`${e}`)
+console.log(e)
+}
+})
+
+//===========menu========
+cmd({
+    pattern: "menu",
+    desc: "To get the menu.",
+    react: "📜",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+    
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+ai: '',
+tools: '',
+search: '',
+fun: '',
+voice: '',
+other: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `
+👋 𝐇𝐄𝐋𝐋𝐎, ${pushname}!
+
+✨ 𝗪𝗲𝗹𝗰𝗼𝗺𝗲 𝘁𝗼 𝗠𝗘𝗧𝗛𝗨_𝗠𝗗 BOT ✨ 
+╭─「 ᴄᴏᴍᴍᴀɴᴅꜱ ᴘᴀɴᴇʟ」
+│◈ яυηтιмє * ${runtime(process.uptime())}
+│◈ σωηєя ηαмє * Nadu
+│◈ σωηєя ηυмвєя * 94741185866
+╰──────────●●►
+╭──────────●●►
+ 📥 *𝗠𝗘𝗧𝗛𝗨_𝗠𝗗 Msg 𝐌𝐞𝐧𝐮*
+  ───────
+hi
+gm
+mk
+gn
+hm
+╰───────────●●►
+
+
+> *BOT BY NADU-MD OFC*`
+
+return await conn.sendMessage(from,{image: {url: `https://i.ibb.co/6RpCBz61/8150.jpg`},caption:madeMenu},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`𝔼𝕣𝕣𝕣𝕠𝕣`)
+}
+})
